fd1b79d31e8313cea3aefbf1f39bdaf0d3287687
elmuerte <git@elmuerte.com>
2025-10-02 21:42:23 +0200

    page titles

--- assets/javascripts/discourse/routes/user-ko-fi-claim.js
+++ assets/javascripts/discourse/routes/user-ko-fi-claim.js
@@ -1,3 +1,11 @@
-import Route from "@ember/routing/route";
+import RestrictedUserRoute from "discourse/routes/restricted-user";
+import { i18n } from "discourse-i18n";
 
-export default class UserKoFiClaimRoute extends Route {}
+export default class UserKoFiClaimRoute extends RestrictedUserRoute {
+  titleToken() {
+    let controller = this.controllerFor(this.router.currentRouteName);
+    let subpageTitle = controller?.subpageTitle;
+    let title = `${i18n("discourse_kofi.title")} - ${i18n("discourse_kofi.user.navigation.claim")}`;
+    return subpageTitle ? `${subpageTitle} - ${title}` : title;
+  }
+}
--- assets/javascripts/discourse/routes/user-ko-fi-payments.js
+++ assets/javascripts/discourse/routes/user-ko-fi-payments.js
@@ -1,6 +1,7 @@
-import Route from "@ember/routing/route";
+import RestrictedUserRoute from "discourse/routes/restricted-user";
+import { i18n } from "discourse-i18n";
 
-export default class UserKoFiPaymentsRoute extends Route {
+export default class UserKoFiPaymentsRoute extends RestrictedUserRoute {
   setupController(controller) {
     if (controller.initialSearch) {
       controller.search = controller.initialSearch;
@@ -11,5 +12,12 @@ export default class UserKoFiPaymentsRoute extends Route {
   resetController(controller) {
     // We don't want to remember the deep link
     controller.set("initialSearch", "");
+  }
+
+  titleToken() {
+    let controller = this.controllerFor(this.router.currentRouteName);
+    let subpageTitle = controller?.subpageTitle;
+    let title = `${i18n("discourse_kofi.title")} ${i18n("discourse_kofi.user.navigation.payments")}`;
+    return subpageTitle ? `${subpageTitle} - ${title}` : title;
   }
 }
