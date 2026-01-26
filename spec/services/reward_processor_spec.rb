bf29e7c9bccf8646be35dbe8215fe6f8c438a66e
elmuerte <git@elmuerte.com>
2026-01-26 19:59:02 +0100

    Test subscription reward reprocessing

--- spec/services/reward_processor_spec.rb
+++ spec/services/reward_processor_spec.rb
@@ -2,38 +2,137 @@
 
 RSpec.describe DiscourseKofi::RewardProcessor do
   fab!(:account) { Fabricate(:kofi_account) }
-  fab!(:payment) { Fabricate(:kofi_payment, account: account) }
-  fab!(:subscription) { Fabricate(:kofi_subscription, account: account) }
-
   fab!(:account2) { Fabricate(:kofi_account) }
-  fab!(:payment2) { Fabricate(:kofi_payment, account: account2) }
 
-  fab!(:payment3) { Fabricate(:kofi_payment) }
+  before(:example) { allow(::Jobs).to receive(:enqueue) }
 
-  fab!(:reward) do
-    Fabricate(:kofi_reward, payment_types: %i[donation subscription])
+  describe "plain rewards" do
+    fab!(:payment) { Fabricate(:kofi_payment, account: account) }
+    fab!(:subscription) { Fabricate(:kofi_subscription, account: account) }
+
+    fab!(:payment2) { Fabricate(:kofi_payment, account: account2) }
+
+    fab!(:payment3) { Fabricate(:kofi_payment) }
+
+    fab!(:reward) do
+      Fabricate(:kofi_reward, payment_types: %i[donation subscription])
+    end
+
+    it "disabled rewards are not reprocessed" do
+      reward.enabled = false
+      reward.save!
+
+      DiscourseKofi::RewardProcessor.reprocess(reward)
+      expect(::Jobs).not_to have_received(:enqueue)
+    end
+
+    it "will schedule payments reprocessing matching a reward" do
+      DiscourseKofi::RewardProcessor.reprocess(reward)
+
+      expect(::Jobs).to have_received(:enqueue)
+        .once
+        .with(
+          DiscourseKofi::Jobs::RewardUser,
+          { payment_id: subscription.id, reward_id: reward.id }
+        )
+        .once
+        .with(
+          DiscourseKofi::Jobs::RewardUser,
+          { payment_id: payment2.id, reward_id: reward.id }
+        )
+
+      expect(::Jobs).to_not have_received(:enqueue).with(
+        DiscourseKofi::Jobs::RewardUser,
+        { payment_id: payment.id }
+      ).with(DiscourseKofi::Jobs::RewardUser, { payment_id: payment3.id })
+    end
   end
 
-  it "will schedule payments reprocessing matching a reward" do
-    allow(::Jobs).to receive(:enqueue)
+  describe "subscription rewards" do
+    fab!(:reward) { Fabricate(:kofi_subscription_reward) }
 
-    DiscourseKofi::RewardProcessor.reprocess(reward)
-
-    expect(::Jobs).to have_received(:enqueue)
-      .once
-      .with(
-        DiscourseKofi::Jobs::RewardUser,
-        { payment_id: subscription.id, reward_id: reward.id }
+    fab!(:payment) do
+      Fabricate(
+        :kofi_subscription,
+        account: account,
+        tier_name: reward.tier_name
       )
-      .once
-      .with(
-        DiscourseKofi::Jobs::RewardUser,
-        { payment_id: payment2.id, reward_id: reward.id }
-      )
+    end
 
-    expect(::Jobs).to_not have_received(:enqueue).with(
-      DiscourseKofi::Jobs::RewardUser,
-      { payment_id: payment.id }
-    ).with(DiscourseKofi::Jobs::RewardUser, { payment_id: payment3.id })
+    fab!(:sub) do
+      sub = DiscourseKofi::Subscription.new
+      sub.user = payment.user
+      sub.reward = reward
+      sub.last_payment = payment
+      sub.update_rewarded_fields
+      sub.save!
+      reward.group.add(sub.user)
+      sub
+    end
+
+    fab!(:payment_no_sub) do
+      Fabricate(
+        :kofi_subscription,
+        account: account,
+        tier_name: reward.tier_name
+      )
+    end
+
+    fab!(:payment_no_account) do
+      Fabricate(:kofi_subscription, tier_name: reward.tier_name)
+    end
+
+    fab!(:payment_old) do
+      Fabricate(
+        :kofi_subscription,
+        account: account,
+        tier_name: reward.tier_name,
+        timestamp: DateTime.now - 6.months
+      )
+    end
+
+    fab!(:payment_other_tier) do
+      Fabricate(:kofi_subscription, account: account2)
+    end
+
+    fab!(:sub_other_tier) do
+      sub = DiscourseKofi::Subscription.new
+      sub.user = payment_other_tier.user
+      sub.reward = reward
+      sub.last_payment = payment_other_tier
+      sub.update_rewarded_fields
+      sub.tier_name = payment_other_tier.tier_name
+      sub.save!
+      reward.group.add(sub.user)
+      sub
+    end
+
+    it "will schedule payments reprocessing matching a subscription reward" do
+      DiscourseKofi::RewardProcessor.reprocess(reward)
+
+      reloaded_sub = DiscourseKofi::Subscription.find_by_id(sub_other_tier.id)
+      # Expiration should have been reduced for the expired subscription handler to deal with
+      expect(reloaded_sub.expires_at).to be < DateTime.now + 90.minutes
+
+      expect(::Jobs).to have_received(:enqueue)
+        .once
+        .with(
+          DiscourseKofi::Jobs::RewardUser,
+          { payment_id: payment.id, reward_id: reward.id }
+        )
+        .once
+        .with(
+          DiscourseKofi::Jobs::RewardUser,
+          { payment_id: payment_no_sub.id, reward_id: reward.id }
+        )
+
+      expect(::Jobs).to_not have_received(:enqueue).with(
+        DiscourseKofi::Jobs::RewardUser,
+        { payment_id: payment_old.id }
+      ).with(
+        DiscourseKofi::Jobs::RewardUser,
+        { payment_id: payment_other_tier.id }
+      )
+    end
   end
 end
