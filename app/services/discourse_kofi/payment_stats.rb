348ca60326c6bfd383f4df0576cf74b9f2725f10
elmuerte <git@elmuerte.com>
2026-02-19 20:35:23 +0100

    corrected leaderboard query + progress bar integration

--- app/services/discourse_kofi/payment_stats.rb
+++ app/services/discourse_kofi/payment_stats.rb
@@ -21,15 +21,17 @@ module DiscourseKofi
         .lease_connection
         .select_all(
           "
-              select min(t.from_name) as from_name, u.id as user_id, sum(t.amount) as total, bool_and(t.is_public) as is_public 
-              from (
-                select coalesce(text(p.user_id), lower(p.email)) as group_id, p.from_name, p.user_id, p.amount, p.is_public
-                from discourse_kofi_payments p
-                where p.timestamp > $1
-                and p.payment_type = any(string_to_array($2, '|'))
-              ) t
-              left outer join users u on u.id = t.user_id
-              group by t.group_id, u.id
+              select 
+                case when p.user_id is not null then text(p.user_id) else p.email end as account,
+                (select n.from_name from discourse_kofi_payments n where n.id = max(p.id)) as from_name,
+                p.user_id as user_id,
+                max(p.id) as last_payment_id, 
+                sum(p.amount) as total, 
+                bool_and(is_public) as is_public
+              from discourse_kofi_payments p
+              where p.timestamp > $1
+              and p.payment_type = any(string_to_array($2, '|'))
+              group by account, user_id
               order by total desc
               limit $3
              ",
@@ -44,7 +46,7 @@ module DiscourseKofi
           if !row["is_public"]
             leaderboard << { anonymous: true }
           elsif row["user_id"]
-            leaderboard << { user_id: row["user_id"] }
+            leaderboard << { user_id: row["user_id"].to_i }
           else
             leaderboard << { name: row["from_name"] }
           end
@@ -79,10 +81,29 @@ module DiscourseKofi
       goal[:progress] = (total / SiteSetting.kofi_goal_amount * 100).floor
 
       PluginStore.set(PLUGIN_NAME, :goal, goal)
+
+      if SiteSetting.kofi_goal_progress_bar_integration
+        update_progress_bar(SiteSetting.kofi_goal_amount, total)
+      end
       goal
     end
 
     def self.calculate_summary
+      # TODO
+    end
+
+    private
+
+    def self.update_progress_bar(target, total)
+      theme =
+        Theme.joins(:remote_theme).find_by(
+          "remote_url = ?",
+          "https://github.com/Canapin/Discourse-progress-bar.git"
+        )
+      return unless theme
+      theme.update_setting(:max_value, target.to_i)
+      theme.update_setting(:current_value, total.to_i)
+      theme.save
     end
   end
 end
